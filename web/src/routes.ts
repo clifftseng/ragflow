aceb1ce591a8165c063e4a87382a2930b1a6d519
balibabu <cike8899@users.noreply.github.com>
2025-09-02 15:47:33 +0800

    Feat: Initialize the data pipeline canvas. #9869 (#9870)
    ### What problem does this PR solve?
    Feat: Initialize the data pipeline canvas. #9869
    
    ### Type of change
    
    
    - [x] New Feature (non-breaking change which adds functionality)

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -42,6 +42,8 @@ export enum Routes {
   AgentShare = '/agent/share',
   ChatShare = `${Chats}/share`,
   UserSetting = '/user-setting',
+  DataFlows = '/data-flows',
+  DataFlow = '/data-flow',
 }
 
 const routes = [
@@ -419,6 +421,22 @@ const routes = [
         component: `@/pages${Routes.ProfileMcp}`,
       },
     ],
+  },
+  {
+    path: Routes.DataFlows,
+    layout: false,
+    component: '@/layouts/next',
+    routes: [
+      {
+        path: Routes.DataFlows,
+        component: `@/pages${Routes.DataFlows}`,
+      },
+    ],
+  },
+  {
+    path: `${Routes.DataFlow}/:id`,
+    layout: false,
+    component: `@/pages${Routes.DataFlow}`,
   },
 ];
 
